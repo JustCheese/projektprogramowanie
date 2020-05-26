089ddd0334d621a39c9a525e955ac45f2c307f26
Hachiro2k20 <dyzio.gogolan@gmail.com>
2020-05-26 16:59:35 +0200

    ustawienia

--- resources/views/ustawienia.blade.php
+++ resources/views/ustawienia.blade.php
@@ -1,31 +1,36 @@
 @extends('layouts.app')
 @section('content')
 
-<h3>Zmień dane</h3>
-<form action="/ustawienia" method="POST">
-    @csrf
-    <input type="password" name="old" placeholder="Stare hasło" required>
-    <input type="password" name="new" placeholder="Nowe hasło" required> 
-    <input type="password" name="newnew" placeholder="Powtórz nowe hasło" required>
-    <input type="submit" value="Zmień" name="haslo">
-</form>
 
+<div class="container sr">
+    <div class="card">
+<h1 class="sr">USTAWIENIA</h1>
 <form action="/ustawienia" method="POST">
     @csrf
-    <input type="text" name="name" placeholder="Nowa nazwa" required>
-    <input type="submit" value="Zmień" name="namee">
+    <input class="dl" type="text" name="name" placeholder="Nowa nazwa" required>
+    <input class="btt" type="submit" value="Zmień" name="namee">
 </form>
+<form action="/ustawienia" method="POST">
+    @csrf
+    <input class="dl" type="text" name="newm" placeholder="Nowy email" required>
+    <input class="btt" type="submit" value="Zmień" name="email">
+</form>
+<br />
+<form action="/ustawienia" method="POST">
+    @csrf
+    <input class="dl2" type="password" name="old" placeholder="Stare hasło" required><br />
+    <input class="dl2" type="password" name="new" placeholder="Nowe hasło" required> <br />
+    <input class="dl2" type="password" name="newnew" placeholder="Powtórz nowe hasło" required><br />
+    <input class="btt" type="submit" value="Zmień" name="haslo">
+</form>
+<br />
 
+<br />
+<br />
 <form action="/ustawienia" method="POST">
     @csrf
-    <input type="text" name="newm" placeholder="Nowy email" required>
-    <input type="submit" value="Zmień email" name="email">
+    <input type="submit" value="USUŃ KONTO" name="delete" style="background-color: red; border: 1px rgb(103, 0, 0); font-weight: 700; margin-bottom: 25px;">
 </form>
-
-<form action="/ustawienia" method="POST">
-    @csrf
-    <input type="submit" value="Usuń konto pajacu" name="delete">
-</form>
-    
-    
+</div>
+</div>
 @endsection
