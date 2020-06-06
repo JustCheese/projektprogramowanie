d0dc3b1b5ac6d9113be9a012a61204ea8894bbf3
JustCheese <daniel.gaida@interia.pl>
2020-06-06 14:10:18 +0200

    polska gurom

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header">Resetuj Hasło</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,14 +18,14 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Adres E-Mail</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>Nie możemy znaleźć użytkownika z takim adresem E-Mail</strong>
                                     </span>
                                 @enderror
                             </div>
@@ -34,7 +34,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    Wyślij link ze zmianą hasła
                                 </button>
                             </div>
                         </div>
